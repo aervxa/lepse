3dc0886d2f5ba5b6f9c79821b98f29410263fa2c
zorvi <aervxa@gmail.com>
2026-04-10 15:20:51 +0000

    Add Authorization header if exists on minos client (api)

--- apps/ariadne/app/plugins/minos.ts
+++ apps/ariadne/app/plugins/minos.ts
@@ -3,10 +3,20 @@ import { registry } from '@lepse/minos/registry'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
+  const token = useCookie('auth_token')
 
   const minos = createTuyau({
     baseUrl: config.public.apiUrl,
     registry,
+    hooks: {
+      beforeRequest: [
+        (request) => {
+          if (token.value) {
+            request.headers.set('Authorization', `Bearer ${token.value}`)
+          }
+        },
+      ],
+    },
   })
 
   return { provide: { minos } }
