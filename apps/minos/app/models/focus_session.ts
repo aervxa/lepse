82916335ce312c18664a296a0aab71f93fe00d16
zorvi <aervxa@gmail.com>
2026-05-05 02:46:32 +0000

    [Minos] fix: update date to be valid in model getDay

--- apps/minos/app/models/focus_session.ts
+++ apps/minos/app/models/focus_session.ts
@@ -1,12 +1,8 @@
 import { FocusSessionSchema } from '#database/schema'
-import { DateTime } from 'luxon'
 
 export default class FocusSession extends FocusSessionSchema {
   public static async getDay(userId: number, date: string) {
-    return this.firstOrCreate(
-      // @ts-ignore | SQLite needs a string for searching instead of DateTime
-      { userId, date: date },
-      { pomoCount: 0, stopwatchMs: 0, date: DateTime.now().toISODate() }
-    )
+    // @ts-ignore | SQLite needs a string for searching instead of DateTime
+    return this.firstOrCreate({ userId, date }, { pomoCount: 0, stopwatchMs: 0, date })
   }
 }
