5c199d4731421e073d6c2f8c0e13f95fa2cf9f7f
zorvi <aervxa@gmail.com>
2026-05-07 03:21:52 +0000

    [Araidne] feat: journal auto-save draft + save (update) journal

--- apps/ariadne/app/pages/app/journal.vue
+++ apps/ariadne/app/pages/app/journal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { formatDate } from '@vueuse/core'
-import { History } from 'lucide-vue-next'
+import { formatDate, useDebounceFn, useLocalStorage } from '@vueuse/core'
+import { History, Save } from 'lucide-vue-next'
 import { getClientDate } from '~/lib/utils'
 
 definePageMeta({
@@ -10,7 +10,7 @@ definePageMeta({
 const today = getClientDate()
 
 const route = useRoute()
-const { journal, fetchJournal } = useDay(today)
+const { journal, fetchJournal, updateJournal } = useDay(today)
 const { journals } = useJournal()
 
 const pastJournals = computed(() => {
@@ -20,12 +20,33 @@ const pastJournals = computed(() => {
 })
 
 const loading = ref(true)
+const draft = useLocalStorage('journal_draft', '')
+const journalBody = ref(draft.value ?? '')
+const journalBodyDirty = computed(() => journalBody.value !== journal.value?.body)
+const saving = ref(false)
+
+const saveJournal = () => {
+  if (!journalBody.value) return
+  saving.value = true
+  updateJournal({ body: journalBody.value }).then(() => {
+    draft.value = ''
+    saving.value = false
+  })
+}
+
 onMounted(async () => {
   await fetchJournal()
   loading.value = false
+  // Update journalBody only if there's no draft (more inportant) and journal body exists
+  if (!draft.value && journal.value?.body) journalBody.value = journal.value.body
 })
-
-const journalBody = ref(journal.value?.body ?? '')
+watch(
+  journalBody,
+  useDebounceFn((val) => {
+    if (journalBodyDirty.value) draft.value = val
+    else draft.value = ''
+  }, 500)
+)
 </script>
 
 <template>
@@ -44,16 +65,28 @@ const journalBody = ref(journal.value?.body ?? '')
       <div class="flex-1 flex flex-col gap-4">
         <p class="text-lg font-semibold">Today's Entry</p>
 
-        <div class="min-h-96 flex">
+        <div class="relative min-h-96 flex">
           <Skeleton v-if="loading" class="flex-1" />
-          <!-- TODO: Save drafts automatically -->
           <Textarea
             v-else
             v-model="journalBody"
             placeholder="What's on your mind today?"
             class="flex-1 p-4"
+            :class="[journalBody.length && 'pb-16']"
           ></Textarea>
-          <!-- TODO: Action to save -->
+          <!-- Actions -->
+          <div class="absolute bottom-4 right-4 flex items-end">
+            <!-- TODO: Actions (markdown) -->
+            <Button
+              v-if="journalBody.length"
+              size="sm"
+              @click="saveJournal"
+              :disabled="saving || !journalBodyDirty"
+            >
+              <Save />
+              {{ journalBodyDirty ? 'Save' : 'Saved' }}
+            </Button>
+          </div>
         </div>
       </div>
 
