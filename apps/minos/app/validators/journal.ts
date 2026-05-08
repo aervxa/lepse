708591a9cacc3e83207fc5a5f56d6faa2d52decb
zorvi <aervxa@gmail.com>
2026-05-08 21:18:19 +0000

    [Minos] twak: allow nulling in journal update

--- apps/minos/app/validators/journal.ts
+++ apps/minos/app/validators/journal.ts
@@ -3,8 +3,8 @@ import vine from '@vinejs/vine'
 export const updateJournalValidator = vine.create(
   vine
     .object({
-      body: vine.string().optional(),
-      mood: vine.number().range([1, 5]).optional(),
+      body: vine.string().optional().nullable(),
+      mood: vine.number().range([1, 5]).optional().nullable(),
     })
     .partial()
 )
