f3eec4f93095ab52ac1b7d60cd0f11aab7299075
zorvi <aervxa@gmail.com>
2026-05-09 16:42:49 +0000

    [Minos] client_date_middleware

---
+++ apps/minos/app/middleware/client_date_middleware.ts
@@ -0,0 +1,34 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import type { NextFn } from '@adonisjs/core/types/http'
+
+export default class ClientDateMiddleware {
+  async handle(ctx: HttpContext, next: NextFn) {
+    /**
+     * Verify existence and validity of date
+     */
+    const raw = ctx.request.header('x-client-date')
+    if (!raw || !/^\d{4}-\d{2}-\d{2}$/.test(raw)) {
+      return ctx.response.badRequest({ error: 'Missing or invalid x-client-date header' })
+    }
+
+    /**
+     * Reject stale dates
+     * allow one day offset to account for timezones
+     */
+    const delta = Date.now() - new Date(raw).getTime()
+    if (Math.abs(delta) / 86_400_000 /* milliseconds for a day */ > 1) {
+      return ctx.response.badRequest({ error: 'Stale x-client-date header' })
+    }
+
+    // Share x-client-date as clientDate
+    ctx.clientDate = raw
+
+    return next()
+  }
+}
+
+declare module '@adonisjs/core/http' {
+  interface HttpContext {
+    clientDate: string | undefined
+  }
+}
--- apps/minos/start/kernel.ts
+++ apps/minos/start/kernel.ts
@@ -38,7 +38,7 @@ router.use([
   () => import('@adonisjs/shield/shield_middleware'),
   () => import('@adonisjs/auth/initialize_auth_middleware'),
   () => import('#middleware/silent_auth_middleware'),
-  () => import('#middleware/initialize_bouncer_middleware')
+  () => import('#middleware/initialize_bouncer_middleware'),
 ])
 
 /**
@@ -46,5 +46,6 @@ router.use([
  * the routes or the routes group.
  */
 export const middleware = router.named({
+  clientDate: () => import('#middleware/client_date_middleware'),
   auth: () => import('#middleware/auth_middleware'),
 })
