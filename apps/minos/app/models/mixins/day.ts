edf26a5853eec82e68d2ff694a271c21aa6b6a2e
zorvi <aervxa@gmail.com>
2026-05-13 17:46:09 +0000

    [Minos] chore: model/mixin/day

--- apps/minos/app/models/mixins/day.ts
+++ apps/minos/app/models/mixins/day.ts
@@ -1,13 +1,10 @@
 import { type BaseModel } from '@adonisjs/lucid/orm'
 import type { NormalizeConstructor } from '@adonisjs/core/types/helpers'
-import { errors } from '@adonisjs/lucid'
 
 export function Day<T extends NormalizeConstructor<typeof BaseModel>>(superclass: T) {
   class MixinClass extends superclass {
     public static async getDayOrFail(userId: number, date: string) {
-      const day = await this.query().where({ userId, date }).first()
-      if (!day) throw new errors.E_ROW_NOT_FOUND()
-      return day
+      return this.query().where({ userId, date }).firstOrFail()
     }
     public static async getDayOrCreate(userId: number, date: string) {
       return this.firstOrCreate({ userId, date } as any, { userId, date } as any)
