2140351a0cd51fd89955b16cc8b8ce182cc5fa89
zorvi <aervxa@gmail.com>
2026-06-04 03:14:32 +0300

    [Ariadne] chore: fix host 0

--- apps/ariadne/nuxt.config.ts
+++ apps/ariadne/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   css: ['./app/assets/css/main.css'],
   // Enables the development server to be discoverable by other devices when running on iOS physical devices
   devServer: {
-    host: '0',
+    host: '0.0.0.0',
   },
   devtools: { enabled: true },
   fonts: {
