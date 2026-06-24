beb441c307338756a0206300a62ae215988af4e5
zorvi <aervxa@gmail.com>
2026-06-24 07:34:23 +0300

    [Ariadne] styling quirks with popovercontent

--- apps/ariadne/app/components/ui/popover/PopoverContent.vue
+++ apps/ariadne/app/components/ui/popover/PopoverContent.vue
@@ -32,7 +32,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
         :class="
           cn(
             unstyled
-              ? 'pointer-events-none *:pointer-events-auto'
+              ? 'pointer-events-none! *:pointer-events-auto!'
               : 'bg-popover data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ring-foreground/5 flex origin-(--reka-popover-content-transform-origin) flex-col gap-4 rounded-2xl p-4 shadow-2xl ring-1 duration-100',
             'text-popover-foreground z-50 w-72 text-sm outline-hidden',
             props.class
@@ -48,5 +48,8 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 <style>
 [data-reka-popper-content-wrapper] {
   pointer-events: none;
+}
+[data-reka-popper-content-wrapper] > * {
+  pointer-events: auto;
 }
 </style>
