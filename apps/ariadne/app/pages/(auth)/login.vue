409939929551e81c5235b3777183fe699f6d29b3
zorvi <aervxa@gmail.com>
2026-07-18 13:19:08 +0300

    [ariadne] chore: update imports

--- apps/ariadne/app/pages/(auth)/login.vue
+++ apps/ariadne/app/pages/(auth)/login.vue
@@ -3,7 +3,6 @@ import { toTypedSchema } from '@vee-validate/zod'
 import { useForm, Field as VeeField } from 'vee-validate'
 import { z } from 'zod'
 import { Eye } from 'lucide-vue-next'
-import { useAuth } from '~/composables/useAuth'
 import { toast } from 'vue-sonner'
 
 const { login } = useAuth()
