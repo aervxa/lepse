6c90ab3b3c3dcd439de2ee7dc156cf5ffe970c15
zorvi <aervxa@gmail.com>
2026-07-21 13:40:11 +0300

    tweak: assign default background for User @beforeCreate

--- apps/clotho/app/models/background.ts
+++ apps/clotho/app/models/background.ts
@@ -1,3 +1,9 @@
 import { BackgroundSchema } from '#database/schema'
 
-export default class Background extends BackgroundSchema {}
+export default class Background extends BackgroundSchema {
+  static async getRandomId() {
+    const bgs = await this.query().select('id')
+    const ids = bgs.map((bg) => bg.id)
+    return ids[Math.floor(Math.random() * ids.length)]
+  }
+}
