58e2c58b6e82646814583358e778ac0f3f6fb2e7
zorvi <aervxa@gmail.com>
2026-08-15 14:40:15 +0300

    chore: use api from env + update workflow

--- apps/lachesis/nuxt.config.ts
+++ apps/lachesis/nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/fonts', 'motion-v/nuxt', 'shadcn-nuxt', 'vue-sonner/nuxt'],
   runtimeConfig: {
     public: {
-      apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3333',
+      apiUrl: process.env.NUXT_PUBLIC_API_URL,
     },
   },
   shadcn: {
