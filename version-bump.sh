dd7f058793742870654e8442a62e5a1cb4eecde2
zorvi <aervxa@gmail.com>
2026-09-05 12:52:17 +0300

    fix version bump script

--- version-bump.sh
+++ version-bump.sh
@@ -3,11 +3,12 @@ set -e
 
 BUMP="${1:-patch}"
 
-npm version "$BUMP" --workspace=apps/lachesis --no-git-tag-version
-VERSION=$(node -p "require('./apps/lachesis/package.json').version")
+cd apps/lachesis
+npm version "$BUMP" --no-git-tag-version
+cd ../..
 
+VERSION=$(node -p "require('./apps/lachesis/package.json').version")
 git add apps/lachesis/package.json
-git add package-lock.json
 git commit -m "v$VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
 git push --follow-tags
