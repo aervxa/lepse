66eae2640429e8623b6e107b22516ad043b2dd87
zorvi <aervxa@gmail.com>
2026-09-05 14:46:58 +0300

    chore: add right favicon

--- apps/atropos/src/app/document.tsx
+++ apps/atropos/src/app/document.tsx
@@ -4,6 +4,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Lepse</title>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" crossOrigin="" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
