6896e214941dcf2cc3252e0d650e5224fab1477a
zorvi <aervxa@gmail.com>
2026-09-24 18:58:01 +0300

    chore/atropos: improve placeholder work in progress - install button

--- apps/atropos/src/worker.tsx
+++ apps/atropos/src/worker.tsx
@@ -3,6 +3,7 @@ import { defineApp } from 'rwsdk/worker'
 
 import { Document } from '@/app/document'
 import { setCommonHeaders } from '@/app/headers'
+import { Button } from '@/app/components/ui/button'
 
 export interface AppContext {
   theme: 'dark' | 'light' | 'system'
@@ -18,12 +19,14 @@ export default defineApp([
   },
   render(Document, [
     route('/', () => (
-      <h2 className="text-center">
-        A work in progress, please go to{' '}
-        <a href="https://os.lepse.app" className="underline">
-          os.lepse.app
-        </a>
-      </h2>
+      <div className="m-0 flex h-dvh flex-col items-center justify-center">
+        <h2 className="text-center">A work in progress, please go to</h2>
+        <Button asChild variant="link">
+          <a href="https://os.lepse.app" className="underline">
+            os.lepse.app
+          </a>
+        </Button>
+      </div>
     )),
   ]),
 ])
